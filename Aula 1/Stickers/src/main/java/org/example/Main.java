21a7c0e298048fdbb6a7ba6f3f478fa45800af47
tainafernandes <tainafefe@gmail.com>
2023-03-28 09:55:55 -0300

    Showing movies by title, picture and rating

--- Aula 1/Stickers/src/main/java/org/example/Main.java
+++ Aula 1/Stickers/src/main/java/org/example/Main.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import org.example.JsonParser.JsonParser;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
         //conexão http
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI address = URI.create(url);
@@ -15,12 +20,18 @@ public class Main {
         var request = HttpRequest.newBuilder(address).GET().build(); //Faço o get
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
         //pegar infos (título, poster, classificação)
-
+        var parser = new JsonParser();
+        List<Map<String, String>> listMovies = parser.parse(body);
 
         //Exibir e manipular os dados
+        for(Map<String, String> movie : listMovies){
+            System.out.println(movie.get("title"));
+            System.out.println(movie.get("image"));
+            System.out.println(movie.get("imDbRating"));
+            System.out.println();
+        }
 
     }
 }
