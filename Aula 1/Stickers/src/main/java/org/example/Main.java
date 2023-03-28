cd75d8117afcdf88cd4ea04343a1ab1ed295ecc6
tainafernandes <tainafefe@gmail.com>
2023-03-28 09:31:57 -0300

    Created conection http

--- Aula 1/Stickers/src/main/java/org/example/Main.java
+++ Aula 1/Stickers/src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        //conexão http
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI address = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(address).GET().build(); //Faço o get
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        //pegar infos (título, poster, classificação)
+
+
+        //Exibir e manipular os dados
+
     }
 }
