ff8815683501e63575a29c8104b157ef8d3094a2
tainafernandes <tainafefe@gmail.com>
2023-03-31 08:52:50 -0300

    Finalization of CRUD

--- linguagens-api/src/main/java/io/github/tainafernandes/linguagensapi/controller/LinguagemController.java
+++ linguagens-api/src/main/java/io/github/tainafernandes/linguagensapi/controller/LinguagemController.java
@@ -3,10 +3,9 @@ package io.github.tainafernandes.linguagensapi.controller;
 import io.github.tainafernandes.linguagensapi.entity.Linguagem;
 import io.github.tainafernandes.linguagensapi.repository.LinguagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -23,6 +22,23 @@ public class LinguagemController {
     @PostMapping("/linguagens")
     public Linguagem create(@RequestBody Linguagem linguagem){
         return repository.save(linguagem);
+    }
+
+    @PutMapping("/linguagens/{id}")
+    public Linguagem update(@PathVariable String id, @RequestBody Linguagem linguagem){
+        if (!repository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        linguagem.setId(id);
+        return repository.save(linguagem);
+    }
+
+    @DeleteMapping("/linguagens/{id}")
+    public void delete(@PathVariable String id){
+        if(!repository.existsById(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        repository.deleteById(id);
     }
 
 }
